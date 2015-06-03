40f1152e98f8806fe93469a8ab261f33e254ad10
Guest User <student@epicodus.com>
2015-06-03 10:02:44 -0700

    specs pass for name, id, cars and alland "all" methods

--- Desktop/vehicles/lib/dealership.rb
+++ Desktop/vehicles/lib/dealership.rb
@@ -1,9 +1,26 @@
 class Dealership
+
+  @@dealerships = []
+
    define_method(:initialize) do |name|
      @name = name
+     @id = @@dealerships.length.+(1)
+     @cars = []
    end
- end
+
 
   define_method(:name) do
     @name
   end
+
+  define_method(:id) do
+    @id
+  end
+
+  define_method(:cars) do
+    @cars
+  end
+  define_singleton_method(:all) do
+    @@dealerships
+  end
+end
--- Desktop/vehicles/spec/dealership_spec.rb
+++ Desktop/vehicles/spec/dealership_spec.rb
@@ -8,3 +8,21 @@ describe('#name') do
     expect(test_dealership.name).to(eq('Tesla Super Cars'))
   end
 end
+
+describe('#id') do
+  it('returns the id of a dealership') do
+    test_dealership = Dealership.new("Tesla Super Cars")
+    expect(test_dealership.id).to(eq(1))
+  end
+end
+describe('#cars') do
+  it('returns an empty array of cars for the dealership') do
+    test_dealership = Dealership.new("Tesla Super Cars")
+    expect(test_dealership.cars).to(eq([]))
+  end
+end
+ describe('.all') do
+   it('is empty at first') do
+     expect(Dealership.all).to(eq([]))
+   end
+ end
